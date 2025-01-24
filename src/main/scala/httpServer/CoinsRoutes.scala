37d351997b0e56b51c3b1f31f0ad9d8ddacacabc
Ribhi El-Zaru <ribhielzaru@gmail.com>
2025-01-24 18:00:08 +0200

    fix some coins repo stuff hehe

--- src/main/scala/httpServer/CoinsRoutes.scala
+++ src/main/scala/httpServer/CoinsRoutes.scala
@@ -1,7 +1,11 @@
 package httpServer
 
 import httpServer.Helpers.handleRepositoryProcess
-import httpServer.Requests.{AddCoinToWalletRequest, UpdateCoinAmountRequest}
+import httpServer.Requests.{
+  AddCoinToWalletRequest,
+  CreateCoinRequest,
+  UpdateCoinAmountRequest
+}
 import models.{CoinValue, UserWithCoins, WalletCoin}
 import repository.CoinsRepository
 import zio.*
@@ -17,17 +21,40 @@ abstract class CoinsRoutes extends RouteContainer {
   private val rootUrl = "coins"
 
   override val routes: Routes[Any, Response] = Routes(
-    Method.POST / rootUrl -> handler { handleAddCoinToWallet(_) },
-    Method.PUT / rootUrl -> handler { (req: Request) =>
-      handleUpdateCoinValue(req)
-    },
-    Method.GET / rootUrl / "coinUuid" / zio.http.uuid("coinUuid") -> handler {
-      (id: UUID, _: Request) => handleLoadCoinByUserId(id)
-    },
+    Method.POST / rootUrl -> handler { handleCreateCoin(_) },
     Method.GET / rootUrl / "userId" / zio.http.uuid("userId") -> handler {
       (id: UUID, _: Request) => handleLoadCoinByUserId(id)
+    },
+    Method.POST / rootUrl / "coinId" / zio.http.uuid(
+      "coinId"
+    ) / "walletId" / zio.http.uuid("walletId") -> handler {
+      (coinId: UUID, walletId: UUID, req: Request) =>
+        handleAddCoinToWallet(coinId, walletId, req)
+    },
+    Method.PUT / rootUrl / "walletCoinId" / zio.http.int(
+      "walletCoinId"
+    ) -> handler { (walletCoinId: Int, req: Request) =>
+      handleUpdateCoinValue(walletCoinId, req)
     }
   )
+
+  private def handleCreateCoin(req: Request): ZIO[Any, Nothing, Response] = {
+    // TODO fix repo collisions
+    handleRepositoryProcess[String](
+      for {
+        requestString <- req.body.asString
+        createCoinRequest <- ZIO.fromEither(
+          requestString.fromJson[CreateCoinRequest]
+        )
+        updateRes <- coinsRepository.createCoin(
+          createCoinRequest.coinId,
+          createCoinRequest.coinName
+        )
+      } yield Right(
+        s"successfully created coin with id [${createCoinRequest.coinId}]"
+      )
+    )
+  }
 
   private def handleLoadCoinByUserId(id: UUID): ZIO[Any, Nothing, Response] = {
     handleRepositoryProcess[UserWithCoins](for {
@@ -44,6 +71,7 @@ abstract class CoinsRoutes extends RouteContainer {
   }
 
   private def handleUpdateCoinValue(
+      walletCoinId: Int,
       req: Request
   ): ZIO[Any, Nothing, Response] = {
     handleRepositoryProcess[String](for {
@@ -52,13 +80,15 @@ abstract class CoinsRoutes extends RouteContainer {
         requestString.fromJson[UpdateCoinAmountRequest]
       )
       updateRes <- coinsRepository.updateCoinOwnedSatoshi(
-        updateCoinAmount.coinId,
+        walletCoinId,
         CoinValue(updateCoinAmount.satoshis)
       )
     } yield Right("successful update"))
   }
 
   private def handleAddCoinToWallet(
+      coinId: UUID,
+      walletId: UUID,
       req: Request
   ): ZIO[Any, Nothing, Response] = {
     handleRepositoryProcess[String](for {
@@ -68,8 +98,8 @@ abstract class CoinsRoutes extends RouteContainer {
       )
       createRes <- coinsRepository
         .addCoinToWallet(
-          addCoinToWallet.coinId,
-          addCoinToWallet.walletId,
+          coinId,
+          walletId,
           CoinValue(addCoinToWallet.satoshis)
         )
         .map(_ => "success")
