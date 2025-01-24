b2727ac556176a29453c37d02822fc32f50150a3
Ribhi El-Zaru <ribhielzaru@gmail.com>
2025-01-24 17:04:07 +0200

    add forgotten coins routes (#9)

--- src/main/scala/httpServer/BaseServer.scala
+++ src/main/scala/httpServer/BaseServer.scala
@@ -4,13 +4,14 @@ import zio.http.*
 import zio.{ZIO, ZIOAppDefault, ZLayer}
 
 trait BaseServer extends ZIOAppDefault {
+  val coinsRoutes: CoinsRoutes
   val userRoutes: UserRoutes
   val walletsRoutes: WalletsRoutes
 
   def startServer: ZIO[Any, Throwable, Nothing] = {
     println("serving")
     Server
-      .serve(userRoutes.routes.++(walletsRoutes.routes))
+      .serve(coinsRoutes.routes.++(userRoutes.routes).++(walletsRoutes.routes))
       .provide(
         Server.live,
         ZLayer.succeed(Server.Config.default.port(8080))
